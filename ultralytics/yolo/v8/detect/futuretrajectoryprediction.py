c650610389fbfaa959bdbb4e53b568e72c7f361b
Chandni12300 <81769024+Chandni12300@users.noreply.github.com>
2023-07-29 21:47:19 +0100

    Add files via upload

---
+++ ultralytics/yolo/v8/detect/futuretrajectoryprediction.py
@@ -0,0 +1,210 @@
+import hydra
+import torch
+import cv2
+import numpy as np
+from filterpy.kalman import KalmanFilter
+from numpy import random
+from pathlib import Path
+from collections import deque
+from ultralytics.yolo.engine.predictor import BasePredictor
+from ultralytics.yolo.utils import DEFAULT_CONFIG, ROOT, ops
+from ultralytics.yolo.utils.checks import check_imgsz
+from ultralytics.yolo.utils.plotting import Annotator
+
+from deep_sort_pytorch.utils.parser import get_config
+from deep_sort_pytorch.deep_sort import DeepSort
+
+palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+data_deque = {}
+kf_dict = {}
+deepsort = None
+
+def init_tracker():
+    global deepsort
+    cfg_deep = get_config()
+    cfg_deep.merge_from_file("deep_sort_pytorch/configs/deep_sort.yaml")
+
+    deepsort = DeepSort(
+        cfg_deep.DEEPSORT.REID_CKPT,
+        max_dist=cfg_deep.DEEPSORT.MAX_DIST,
+        min_confidence=cfg_deep.DEEPSORT.MIN_CONFIDENCE,
+        nms_max_overlap=cfg_deep.DEEPSORT.NMS_MAX_OVERLAP,
+        max_iou_distance=cfg_deep.DEEPSORT.MAX_IOU_DISTANCE,
+        max_age=cfg_deep.DEEPSORT.MAX_AGE,
+        n_init=cfg_deep.DEEPSORT.N_INIT,
+        nn_budget=cfg_deep.DEEPSORT.NN_BUDGET,
+        use_cuda=True
+    )
+
+def xyxy_to_xywh(*xyxy):
+    """Calculates the relative bounding box from absolute pixel values."""
+    bbox_left = min([xyxy[0].item(), xyxy[2].item()])
+    bbox_top = min([xyxy[1].item(), xyxy[3].item()])
+    bbox_w = abs(xyxy[0].item() - xyxy[2].item())
+    bbox_h = abs(xyxy[1].item() - xyxy[3].item())
+    x_c = (bbox_left + bbox_w / 2)
+    y_c = (bbox_top + bbox_h / 2)
+    w = bbox_w
+    h = bbox_h
+    return x_c, y_c, w, h
+
+def compute_color_for_labels(label):
+    """Simple function that adds fixed color depending on the class."""
+    if label == 0:  # person
+        color = (85, 45, 255)
+    elif label == 2:  # Car
+        color = (222, 82, 175)
+    elif label == 3:  # Motobike
+        color = (0, 204, 255)
+    elif label == 5:  # Bus
+        color = (0, 149, 255)
+    else:
+        color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+    return tuple(color)
+
+def draw_boxes(img, bbox, names, object_id, identities=None, offset=(0, 0)):
+    for i, box in enumerate(bbox):
+        # Calculate center
+        x1, y1, x2, y2 = [int(i) for i in box]
+        center = ((x1 + x2) / 2, (y1 + y2) / 2)
+
+        # Define color for the prediction circle
+        color = (0, 0, 255)  # Red color in BGR
+
+        # get ID of object
+        id = int(identities[i]) if identities is not None else 0
+
+        # Initialize Kalman filter for new object
+        if id not in kf_dict:
+            kf = KalmanFilter(dim_x=4, dim_z=2)
+            kf.x = np.array([center[0], center[1], 0., 0.])  # initial state (location and velocity)
+            kf.F = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]])  # state transition matrix
+            kf.H = np.array([[1, 0, 0, 0], [0, 1, 0, 0]])  # measurement function
+            kf.R *= 500.  # measurement uncertainty
+            kf.P *= 1000.  # initial uncertainty
+            kf.Q = np.eye(4)  # process uncertainty
+            kf_dict[id] = kf
+        else:
+            kf = kf_dict[id]
+
+        # Update Kalman filter with current measurement
+        kf.predict()
+        kf.update(np.array([center[0], center[1]]))
+
+        # Predict future position
+        if kf.x is not None:  # check if state vector x is not None
+            future_position = (int(kf.x[0]), int(kf.x[1]))
+            # Calculate the direction vector from current to future position (fix: reverse the subtraction order)
+            direction_vector = (future_position[0] - int(center[0]), future_position[1] - int(center[1]))
+            # Draw line with arrowed line from future position to current position
+            cv2.arrowedLine(img, future_position, (int(center[0]), int(center[1])), color, 2, tipLength=2)
+
+        # Draw bounding box
+        color = compute_color_for_labels(object_id[i])
+        label = f'{id}:{names[object_id[i]]}'
+        UI_box(box, img, label=label, color=color, line_thickness=2)
+
+
+def UI_box(x, img, color=None, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+
+        cv2.rectangle(img, (c1[0], c1[1] - t_size[1] - 3), (c1[0] + t_size[0], c1[1] + 3), color, -1)
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
+class DetectionPredictor(BasePredictor):
+    def get_annotator(self, img):
+        return Annotator(img, line_width=self.args.line_thickness, example=str(self.model.names))
+
+    def preprocess(self, img):
+        img = torch.from_numpy(img).to(self.model.device)
+        img = img.half() if self.model.fp16 else img.float()  # Convert to fp16 if self.model.fp16 is True
+        img /= 255.0  # Normalize pixel values from 0 - 255 to 0.0 - 1.0
+        
+        return img
+
+
+    def postprocess(self, preds, img, orig_img):
+        preds = ops.non_max_suppression(preds,
+                                        self.args.conf,
+                                        self.args.iou,
+                                        agnostic=self.args.agnostic_nms,
+                                        max_det=self.args.max_det)
+
+        for i, pred in enumerate(preds):
+            shape = orig_img[i].shape if self.webcam else orig_img.shape
+            pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], shape).round()
+
+        return preds
+
+    def write_results(self, idx, preds, batch):
+        p, im, im0 = batch
+        all_outputs = []
+        log_string = ""
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+        self.seen += 1
+        im0 = im0.copy()
+        if self.webcam:  # batch_size >= 1
+            log_string += f'{idx}: '
+            frame = self.dataset.count
+        else:
+            frame = getattr(self.dataset, 'frame', 0)
+
+        self.data_path = p
+        save_path = str(self.save_dir / p.name)  # im.jpg
+        self.txt_path = str(self.save_dir / 'labels' / p.stem) + ('' if self.dataset.mode == 'image' else f'_{frame}')
+        log_string += '%gx%g ' % im.shape[2:]  # print string
+        self.annotator = self.get_annotator(im0)
+
+        det = preds[idx]
+        all_outputs.append(det)
+        if len(det) == 0:
+            return log_string
+        for c in det[:, 5].unique():
+            n = (det[:, 5] == c).sum()  # detections per class
+            log_string += f"{n} {self.model.names[int(c)]}{'s' * (n > 1)}, "
+        # write
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        xywh_bboxs = []
+        confs = []
+        oids = []
+        outputs = []
+        for *xyxy, conf, cls in reversed(det):
+            x_c, y_c, bbox_w, bbox_h = xyxy_to_xywh(*xyxy)
+            xywh_obj = [x_c, y_c, bbox_w, bbox_h]
+            xywh_bboxs.append(xywh_obj)
+            confs.append([conf.item()])
+            oids.append(int(cls))
+        xywhs = torch.Tensor(xywh_bboxs)
+        confss = torch.Tensor(confs)
+        outputs = deepsort.update(xywhs, confss, oids, im0)
+        if len(outputs) > 0:
+            bbox_xyxy = outputs[:, :4]
+            identities = outputs[:, -2]
+            object_id = outputs[:, -1]
+            draw_boxes(im0, bbox_xyxy, self.model.names, object_id, identities)
+
+        return log_string
+
+@hydra.main(version_base=None, config_path=str(DEFAULT_CONFIG.parent), config_name=DEFAULT_CONFIG.name)
+def predict(cfg):
+    init_tracker()
+    cfg.model = cfg.model or "yolov8n.pt"
+    cfg.imgsz = check_imgsz(cfg.imgsz, min_dim=2)  # check image size
+    cfg.source = cfg.source if cfg.source is not None else ROOT / "assets"
+    predictor = DetectionPredictor(cfg)
+    predictor()
+
+if __name__ == "__main__":
+    predict()
+
+
+
